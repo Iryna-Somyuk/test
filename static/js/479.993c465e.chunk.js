2cccb852bb12d29c9be2b5511e7aae0547b95a1e
Iryna-Somyuk <107417578+Iryna-Somyuk@users.noreply.github.com>
2023-05-19 07:52:38 +0000

    Deploying to gh-pages from @ Iryna-Somyuk/test@8e5a9ecf42830eb402e494eac6dd36a0690ad9d0 🚀

---
+++ static/js/479.993c465e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[479],{3479:function(t,e,r){r.d(e,{KT0:function(){return o}});var n=r(9983);function o(t){return(0,n.w_)({tag:"svg",attr:{fill:"none",viewBox:"0 0 24 24",strokeWidth:"1.5",stroke:"currentColor","aria-hidden":"true"},child:[{tag:"path",attr:{strokeLinecap:"round",strokeLinejoin:"round",d:"M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"}}]})(t)}}}]);
+//# sourceMappingURL=479.993c465e.chunk.js.map
